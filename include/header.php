4366c977e785f6af1f36453a549ff130ad0fd0f2
Tech <techdept@educul.in>
2026-04-28 15:59:28 +0530

    eee

--- include/header.php
+++ include/header.php
@@ -16,42 +16,38 @@
 --------------------------- header ---------------------
 --------------------------------------------------------> 
       <header id="myHeader" class="position-fixed z-3 w-100 px-lg-3 --transition-ease">
-         <nav class="navbar navbar-expand-lg bg-body-tertiary bg-transparent py-3 --transition-ease">
+         <nav class="navbar navbar-expand-lg bg-body-tertiary bg-transparent py-1 --transition-ease">
             <div class="container position-relative">
                <div class="head">
                   <a class="navbar-brand" href="index.php">
                   <img src="./images/logo.png" class="logo-img --transition-ease">
-                  <img src="images/footerlogo.png" class="footerlogo logo-img --transition-ease" alt="logo"/>
+                  <img src="images/logo.png" class="footerlogo logo-img --transition-ease" alt="logo"/>
                   </a>
                </div>
                <div class="headmenu">
                   <button class="navbar-toggler collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                     <!-- <svg xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
-                        <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5"/>
-                     </svg> -->
+            
                      <img src="images/menu2.png" alt="menu2"/>
-                     <img class="d-none" src="images/menu2w.png" alt="menu2"/>
+                     <!-- <img class="d-none" src="images/menu2w.png" alt="menu2"/> -->
                  
-                     <!-- <svg class="d-none" xmlns="http://www.w3.org/2000/svg" width="27" height="27" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
-                        <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
-                     </svg> -->
+                
                   </button>
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
                      <div class="container" >
                         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="#">Home</a></li>
-                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="#">About Us</a></li>
-                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="#">Packages</a></li>
-                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="#">Gallery</a></li>
-                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="#">Blog</a></li>
-                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="#">Contact Us</a></li>
+                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="index.php">Home</a></li>
+                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="index.php#about">About</a></li>
+                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="index.php#packages">Packages</a></li>
+                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="index.php#whyus">Why Us?</a></li>
+                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="index.php#areas">Areas We Serve</a></li>
+                           <li class="nav-item"><a class="nav-link text-black px-xl-3" href="index.php#contact">Contact Us</a></li>
                         </ul>
                         <a href="#" class="d-block mb-3 d-lg-none text-white btn btn-lg rounded rounded-2 text-uppercase px-3 px-xl-4 align-items-center justify-content-center fw-bold" >Contact Us <img src="images/arrow.png" class="ms-3" alt="arrow" height="12px" width="12px"/></a>
                      </div>
                   </div>
                </div>
                <div class="head text-end">
-                  <a href="#" class="text-white btn btn-lg rounded rounded-2 text-uppercase px-3 px-xl-4 d-inline-flex align-items-center justify-content-center fw-bold" >Contact Us <img src="images/arrow.png" class="ms-3" alt="arrow" height="12px" width="12px"/></a>
+                  <a href="https://api.whatsapp.com/send/?phone=0414723129&text=Hi, I’d like to book a driving lesson with James Driving School. Can you please share available slots?" class="text-white btn btn-lg rounded rounded-2 text-uppercase px-2 px-xxl-3 d-inline-flex align-items-center justify-content-center fw-bold" >Contact Us <img src="images/arrow.png" class="ms-3" alt="arrow" height="12px" width="12px"/></a>
                </div>
             </div>
          </nav>
